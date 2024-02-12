320bcb3ff996ae73271edfa89544db87c39181f7
Elias Ablan <eliasablan93@gmail.com>
2024-02-12 12:45:30 -0400

    disable vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+// import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -50,7 +50,7 @@ export default async function RootLayout({
           </div>
         </ThemeProvider>
         <Analytics />
-        <SpeedInsights />
+        {/* <SpeedInsights /> */}
       </body>
     </html>
   )
