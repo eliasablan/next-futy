1126417f9b337f38ca0cda6711cbe5ed5df4affe
Elias Ablan <eliasablan93@gmail.com>
2024-02-12 21:03:45 -0400

    tooltip added to sidebar

--- src/components/SidebarLeagueLink.tsx
+++ src/components/SidebarLeagueLink.tsx
@@ -1,57 +1,53 @@
 'use client'
-import React from 'react'
+import React, { useContext } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { Button } from './ui/button'
-// import {
-//   Tooltip,
-//   TooltipContent,
-//   TooltipProvider,
-//   TooltipTrigger,
-// } from '@/components/ui/tooltip'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 import { League } from '@/lib/types/league'
 import { cn } from '@/lib/utils'
+import { MenuContext } from './MenuProvider'
 
 export function SidebarLeagueLink(league: League) {
+  const { setMobileMenuOpen } = useContext(MenuContext)
   const pathname = usePathname()
   return (
-    // <TooltipProvider>
-    //   <Tooltip defaultOpen={true}>
-    //     <TooltipTrigger asChild>
-    <Button
-      asChild
-      variant="ghost"
-      size="icon"
-      className={cn(
-        'h-14 w-14',
-        pathname === `/leagues/${league.code}` &&
-          'border border-ring dark:border-border'
-      )}
-    >
-      <Link href={`/leagues/${league.code}`}>
-        <Image
-          src={league.emblem}
-          alt={league.name}
-          width={40}
-          height={40}
-        />
-      </Link>
-    </Button>
-    //     </TooltipTrigger>
-    //     <TooltipContent
-    //       align="center"
-    //       side="right"
-    //       sideOffset={0}
-    //       className="z-[9999]"
-    //       // alignOffset={5}
-    //       // avoidCollisions={true}
-    //     >
-    //       <p>{league.name}</p>
-    //       {/* <TooltipArrow /> */}
-    //     </TooltipContent>
-    //   </Tooltip>
-    // </TooltipProvider>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            asChild
+            variant="ghost"
+            size="icon"
+            className={cn(
+              'h-14 w-14',
+              pathname === `/leagues/${league.code}` && 'border bg-accent'
+            )}
+          >
+            <Link
+              onClick={() => setMobileMenuOpen(false)}
+              href={`/leagues/${league.code}`}
+            >
+              <Image
+                src={league.emblem}
+                alt={league.name}
+                width={40}
+                height={40}
+              />
+            </Link>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="text-lg" align="center" side="right">
+          {league.name}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
