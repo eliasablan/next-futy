7fbea85785a58ad1f2512b74bc8cb8456404857e
Elias Ablan <eliasablan93@gmail.com>
2024-02-14 02:31:40 -0400

    Add LeaguesCard component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,15 @@
-import { fetchLeagues } from '@/lib/data/queries'
-import { League } from '@/lib/types/league'
-
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-} from '@/components/ui/card'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import Image from 'next/image'
-import { cn } from '@/lib/utils'
+import LeaguesCard from '@/components/LeaguesCard'
 import TeamsCard from '@/components/TeamsCard'
 import { SearchParams } from '@/lib/types/searchParams'
 
-export default async function Home({
+export default function Home({
   searchParams,
 }: {
   searchParams: SearchParams
 }) {
-  const leagues: League[] = await fetchLeagues()
-
   return (
     <div className="grid grid-cols-1 gap-6 p-6 lg:grid-cols-2 2xl:grid-cols-3">
-      <section className="col-span-1">
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-lg">Leages</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <ScrollArea>
-              <div className="mt-2 text-center">
-                {leagues &&
-                  leagues.map((league) => (
-                    <Button
-                      key={league.id}
-                      asChild
-                      variant="ghost"
-                      size="icon"
-                      className={cn('h-14 w-14')}
-                    >
-                      <Link href={`/leagues/${league.code}`}>
-                        <Image
-                          src={league.emblem}
-                          alt={league.name}
-                          width={40}
-                          height={40}
-                        />
-                      </Link>
-                    </Button>
-                  ))}
-              </div>
-            </ScrollArea>
-          </CardContent>
-        </Card>
-      </section>
+      <LeaguesCard />
       <TeamsCard searchParams={searchParams} />
     </div>
   )
---
+++ src/components/LeaguesCard.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { League } from '@/lib/types/league'
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from '@/components/ui/card'
+import { Button } from './ui/button'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { fetchLeagues } from '@/lib/data/queries'
+
+export default async function LeaguesCard() {
+  const leagues: League[] = await fetchLeagues()
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-lg">Leagues</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="relative">
+          <div className="xs:grid-cols-3 mx-auto mt-2 grid w-full grid-cols-2 justify-items-center sm:grid-cols-4">
+            {leagues &&
+              leagues.map((league) => (
+                <Button
+                  key={league.id}
+                  asChild
+                  variant="ghost"
+                  size="icon"
+                  className="h-40 w-full px-2"
+                >
+                  <Link
+                    className="flex flex-col text-center"
+                    href={`/leagues/${league.code}`}
+                  >
+                    {league.emblem && (
+                      <Image
+                        src={league.emblem}
+                        alt={league.name}
+                        width={40}
+                        height={40}
+                      />
+                    )}
+                    <span className="mt-5 w-full overflow-hidden text-ellipsis text-wrap ">
+                      {league.name ? league.name : 'Not found'}
+                    </span>
+                  </Link>
+                </Button>
+              ))}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
