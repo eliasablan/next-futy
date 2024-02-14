c679d5a79d7ea2fdfd0fbc34cc39c156bb6c4d2a
Elias Ablan <eliasablan93@gmail.com>
2024-02-14 03:45:32 -0400

    Add navigation buttons to the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,29 @@
+'use client'
 import React from 'react'
 
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Button } from './ui/button'
+
 export default function Header() {
-  return <header className="flex h-16 border-b p-6"></header>
+  const pathname = usePathname()
+
+  return (
+    <header className="my-auto flex h-16 items-center border-b px-6">
+      <Button
+        variant={pathname !== '/leagues' ? 'link' : 'secondary'}
+        asChild
+      >
+        <Link href="/leagues">Leagues</Link>
+      </Button>
+
+      <Button
+        variant={pathname !== '/teams' ? 'link' : 'secondary'}
+        asChild
+      >
+        <Link href="/teams">Teams</Link>
+      </Button>
+    </header>
+  )
 }
