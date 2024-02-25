0d99ce731010951b550fbfeba7c866fa2ea6dc29
Elias Ablan <eliasablan93@gmail.com>
2024-02-25 12:12:15 -0400

    make team page public

--- src/app/teams/[id]/page.tsx
+++ src/app/teams/[id]/page.tsx
@@ -1,8 +1,4 @@
 import React from 'react'
-
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
-
 import TeamCard from '@/components/TeamCard'
 import MatchesCard from '@/components/MatchesCard'
 import PlayersCard from '@/components/PlayersCard'
@@ -11,11 +7,6 @@ import { fetchTeam } from '@/lib/data/queries'
 import { Team } from '@/lib/types/team'
 
 const TeamPage = async ({ params }: { params: { id: number } }) => {
-  const session = await getServerSession()
-  if (!session || !session.user) {
-    redirect('/api/auth/signin')
-  }
-
   const team: Team = await fetchTeam(params.id)
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
